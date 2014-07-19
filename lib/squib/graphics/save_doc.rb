5ac32c9710f294e39a5b06a0794573b84c7f1575
Andy Meneely <andy@se.rit.edu>
2014-07-18 23:14:29 -0400

    Still working...

--- lib/squib/graphics/save_doc.rb
+++ lib/squib/graphics/save_doc.rb
@@ -1,25 +1,34 @@
 module Squib
   class Deck
 
-    def save_pdf(file: 'deck.pdf', dir: '_output')
-      #paper is 8.5x11
-      # PDF points are 1/72 of an inch
-      width = 8.5 * @dpi
-      height = 11 * @dpi
+    def save_pdf(file: 'deck.pdf', dir: '_output', margin: 50, gap: 0, trim: 0)
+      width = 11 * @dpi
+      height = 8.5 * @dpi
       cc = Cairo::Context.new(Cairo::PDFSurface.new("#{dir}/#{file}", width, height))
-      margin = 50 #paper margin in px
-      x = margin
-      y = margin
+      x = margin ; y = margin
       @cards.each_with_index do |card, i|
         cc.move_to(x,y)
+        surface = trim(card.cairo_surface, trim, @width, @height)
         cc.set_source(card.cairo_surface, x,y)
-        x = (card.width * i)
-        if x > (width - margin)
-          x = margin 
-          y += card.height
-        end
         cc.paint
+        x += card.width + gap
+        if x > (width - card.width - margin)
+          x = margin 
+          y += card.height + gap
+          if y > (height - card.height - margin)
+            x = margin ; y = margin
+            cc.show_page #next page
+          end
+        end
+
       end
+    end
+
+    def trim(surface, trim, width, height)
+      if trim > 0
+        #Cairo::ImageSurface.new()
+      end
+      surface
     end
 
   end
--- samples/save-pdf.rb
+++ samples/save-pdf.rb
@@ -1,15 +1,13 @@
 #!/usr/bin/env ruby
 require 'squib'
 
-names = ['Thief', 'Grifter', 'Mastermind']
-
 Squib::Deck.new(width: 825, height: 1125, cards: 16) do
-  background color: :white
+  background color: :gray
   rect x: 38, y: 38, width: 750, height: 1050, x_radius: 38, y_radius: 38
 
-  text str: names, x: 220, y: 78, font: 'Arial 54'
+  text str: (1..16).to_a, x: 220, y: 78, font: 'Arial 54'
 
-  save_pdf file: "sample.pdf", dir: "_output"
+  save_pdf file: "sample.pdf", margin: 50, gap: 5, trim: 38
 end
 
 puts "Done!"
