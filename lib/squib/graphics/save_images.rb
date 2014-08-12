fe438f905ba19cfcf9b3024a6196d5719e873b0e
Andy Meneely <andy@se.rit.edu>
2014-08-11 20:28:20 -0400

    starting rotate

--- lib/squib/graphics/save_images.rb
+++ lib/squib/graphics/save_images.rb
@@ -3,8 +3,31 @@ module Squib
 
     # :nodoc:
     # @api private 
-    def save_png(i, dir, prefix)
-      cairo_context.target.write_to_png("#{dir}/#{prefix}#{i}.png")
+    def save_png(i, dir, prefix, rotate)
+      @cairo_surface.flush
+      if rotate
+        surface = rotated_image
+      else
+        surface = @cairo_surface
+      end
+      write_png(surface, i, dir, prefix)
+    end
+
+    def rotated_image
+      rotated_cc = Cairo::Context.new(Cairo::ImageSurface.new(@height, @width) )
+      rotated_cc.save
+      rotated_cc.set_source(@cairo_surface)
+      rotated_cc.move_to(0,0)
+      rotated_cc.rotate(1.5 * Math::PI)
+      rotated_cc.paint
+      rotated_cc.restore
+      rotated_cc.target
+
+    end
+    # :nodoc:
+    # @api private 
+    def write_png(surface, i, dir, prefix)
+      surface.write_to_png("#{dir}/#{prefix}#{i}.png")
     end
 
   end
